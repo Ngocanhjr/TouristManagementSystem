58320fb7be28b2e67346213fc85a445379db25d0
Lengocanh <Lengocanh3524@gmail.com>
2024-10-24 20:57:06 +0700

    demo mvc

--- src/main/java/com/myctu/touristmanagementsystem/controller/LoginController.java
+++ src/main/java/com/myctu/touristmanagementsystem/controller/LoginController.java
@@ -6,31 +6,49 @@ package com.myctu.touristmanagementsystem.controller;
 
 import com.myctu.touristmanagementsystem.dao.ChuDichVuDAO;
 import com.myctu.touristmanagementsystem.dao.KhachDuLichDAO;
+import com.myctu.touristmanagementsystem.model.DangNhap;
 import com.myctu.touristmanagementsystem.view.AdminView;
 import com.myctu.touristmanagementsystem.view.LoginView;
 import com.myctu.touristmanagementsystem.view.UserView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
  * @author ASUS
  */
 public class LoginController {
+
     private LoginView loginView;
-    
+
     private AdminView adminView;
-    
+
     private UserView userView;
-    
+
     private ChuDichVuDAO chuDichVuDAO;
-    
+
     private KhachDuLichDAO khachDuLichDAO;
 
     public LoginController(LoginView loginView) {
         this.loginView = loginView;
+        loginView.addDangNhapListener(new dangNhaplistener());
     }
+
     
-    public void showLoginView(){
+    public void showLoginView() {
         loginView.setVisible(true);
     }
-    
+
+//    public boolean 
+    class dangNhaplistener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            DangNhap dt = loginView.getDangNhap();
+            if (dt != null) {
+                
+            }
+        }
+
+    }
 }
---
+++ src/main/java/com/myctu/touristmanagementsystem/model/DangNhap.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myctu.touristmanagementsystem.model;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DangNhap {
+    private String tenDangNhap;
+    
+    private String matKhau;
+
+    public DangNhap() {
+    }
+
+    public DangNhap(String tenDangNhap, String matKhau) {
+        this.tenDangNhap = tenDangNhap;
+        this.matKhau = matKhau;
+    }
+
+    public String getTenDangNhap() {
+        return tenDangNhap;
+    }
+
+    public void setTenDangNhap(String tenDangNhap) {
+        this.tenDangNhap = tenDangNhap;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+    
+    
+}
